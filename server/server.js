77ccb4638bfff3d22bccff4329880979f085c50f
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-12-18 21:43:57 +0530

    Rename file

--- server/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-
-const app = express();
-
-const PORT = process.env.PORT || 3005;
-app.listen(PORT, () => {
-  console.log(`Server is up and listening at ${PORT}`);
-});
