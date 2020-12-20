0377a1dcbc53494677ecb8668bf3c50b069b7605
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-12-20 22:29:15 +0530

    feat(server): add cors support

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 
 const morgan = require('morgan');
 const express = require('express');
+const cors = require('cors');
 const db = require('./db');
 const { setupRoutes } = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
