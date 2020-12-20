77fae9e70f88d44262f48656dfd09ddb86fb5418
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-12-20 22:07:36 +0530

    feat(client): add context api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 import Home from './routes/Home';
 import RestaurantDetailsPage from './routes/RestaurantDetailsPage';
 import UpdatePage from './routes/UpdatePage';
 
 const App = () => {
   return (
-    <div className='container'>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route
-            exact
-            path='/restaurants/:id'
-            component={RestaurantDetailsPage}
-          />
-          <Route exact path='/restaurants/:id/update' component={UpdatePage} />
-        </Switch>
-      </Router>
-    </div>
+    <RestaurantsContextProvider>
+      <div className='container'>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route
+              exact
+              path='/restaurants/:id'
+              component={RestaurantDetailsPage}
+            />
+            <Route
+              exact
+              path='/restaurants/:id/update'
+              component={UpdatePage}
+            />
+          </Switch>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
   );
 };
 
